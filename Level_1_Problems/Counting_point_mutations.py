099603b68993d44e5590c37c8858563005ca504d
Edward Blake <128470247+eblake90@users.noreply.github.com>
2023-10-21 09:00:43 +0200

    Update Counting_point_mutations.py

--- Level_1_Problems/Counting_point_mutations.py
+++ Level_1_Problems/Counting_point_mutations.py
@@ -1,9 +1,15 @@
+# https://rosalind.info/problems/hamm/
+# Given: Two DNA strings s and t of equal length (not exceeding 1 kbp).
+# Return: The Hamming distance d_H (s,t).
+
+
 with open("C:\\Users\\eblak\\Downloads\\rosalind_hamm.txt", "r") as  file:
     s = file.readline().strip()
     t = file.readline().strip()
 
 hamming_distance = 0
 
+# Finding mismatches along both strings
 for i in range(0, len(s)):
     if s[i] != t[i]:
         hamming_distance += 1
