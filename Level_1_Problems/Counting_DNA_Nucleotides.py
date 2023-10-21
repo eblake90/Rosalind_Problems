c47773e27d7d320954e04bd65e6e532cfc049853
Edward Blake <128470247+eblake90@users.noreply.github.com>
2023-10-21 08:50:19 +0200

    Update Counting_DNA_Nucleotides.py

--- Level_1_Problems/Counting_DNA_Nucleotides.py
+++ Level_1_Problems/Counting_DNA_Nucleotides.py
@@ -1,3 +1,7 @@
+# https://rosalind.info/problems/dna/
+# Given: A DNA string s of length at most 1000 nt
+# Return: Four integers (separated by spaces) counting the respective number of times that the symbols 'A', 'C', 'G', and 'T' occur in s.
+
 import collections
 import  random
 
