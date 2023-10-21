75437eabbb2f6a4fccf2b7808dcb247da5df6e4a
Edward Blake <128470247+eblake90@users.noreply.github.com>
2023-10-21 09:01:39 +0200

    Update Finding_a_Motif_in_DNA.py

--- Level_1_Problems/Finding_a_Motif_in_DNA.py
+++ Level_1_Problems/Finding_a_Motif_in_DNA.py
@@ -1,3 +1,7 @@
+# https://rosalind.info/problems/subs/
+# Given: Two DNA strings s and t (each of length at most 1 kbp).
+# Return: All locations of t as a substring of s.
+
 
 with open("C:\\Users\\eblak\\Downloads\\rosalind_subs.txt", "r") as  file:
     s = file.readline().strip()
